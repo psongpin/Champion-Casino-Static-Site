61311f69dae87d410862ba3df17a747c5f542354
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2017-08-15 13:31:32 +0800

    Mobile Lobby Nav
    Create new copy of lobby nav for mobile

--- js/script.js
+++ js/script.js
@@ -17,13 +17,22 @@ $(document).ready(function(){
   $('.hms-next').click(function(){ $('.homepage-main-slider-parent').slick('slickNext'); });
 
   // Lobby Navigation =================================================
-  $('.lobby-navigation').slick({
+  $('.lobby-navigation.desktop').slick({
     arrows: false,
     infinite: true,
     slidesToShow: 8,
     responsive: [
       { breakpoint: 991, settings: { slidesToShow: 5, slidesToScroll: 5, initialSlide: 8, arrows: true } },
       { breakpoint: 767, settings: { slidesToShow: 3, slidesToScroll: 3, initialSlide: 8, arrows: true } }
+    ]
+  });
+  $('.lobby-navigation.mobile').slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 8,
+    responsive: [
+      { breakpoint: 991, settings: { slidesToShow: 5, slidesToScroll: 5, arrows: true } },
+      { breakpoint: 767, settings: { slidesToShow: 3, slidesToScroll: 3, arrows: true } }
     ]
   });
 
